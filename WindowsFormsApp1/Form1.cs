b43ad928f15c7e1b9b2da9acb9545e06abc51928
Pavel Fiala <fialapa.02@spst.eu>
2021-05-05 09:06:45 +0200

    DalsiZmena

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Ahoj xxxxxxxx");
+            MessageBox.Show("Ahoj xxxxxxxx122");
         }
     }
 }
